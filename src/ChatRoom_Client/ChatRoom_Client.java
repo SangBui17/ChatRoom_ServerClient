daab8fc9c42025bbcb1549f533c4404e07e38973
SangBui17 <sangbui0367@gmail.com>
2023-11-10 10:28:57 +0700

    ChatRoom Project

---
+++ src/ChatRoom_Client/ChatRoom_Client.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ChatRoom_Client;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ChatRoom_Client {
+     public static void main(String[] args) {
+       ChatClient chatClient = new ChatClient();
+       chatClient.startClient();
+    }
+    
+}
